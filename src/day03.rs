5f586c68820b36e235bae5d23e3404e6bf5c070a
rxdps93 <dpsirott@gmail.com>
2023-12-10 22:56:09 -0500

    testing out an approach

--- src/day03.rs
+++ src/day03.rs
@@ -3,35 +3,68 @@ use regex::{Regex, Match};
 
 pub fn part01() {
     let _lines = input_as_str_vec("day03");
-    let test = vec![
-        "467..114..",
-        "...*......",
-        "..35..633.",
-        "......#...",
-        "617*......",
-        ".....+.58.",
-        "..592.....",
-        "......755.",
-        "...$.*....",
-        ".664.598.."
+    let mut test: Vec<String> = vec![
+        "467..114..".to_string(),
+        "...*......".to_string(),
+        "..35..633.".to_string(),
+        "......#...".to_string(),
+        "617*......".to_string(),
+        ".....+.58.".to_string(),
+        "..592.....".to_string(),
+        "......755.".to_string(),
+        "...$.*....".to_string(),
+        ".664.598..".to_string()
     ];
 
-    let mut nums: Vec<Vec<Match>> = Vec::new();
-    let re = Regex::new(r"(?<num>\d+)").unwrap();
-    for line in test {
-        // println!("Line {idx}: {line}");
+    let blank = str::repeat(".", test[0].len());
+    test.splice(0..0, vec![blank.clone()].iter().cloned());
+    test.push(blank);
+    test = test.iter().map(|line| {
+        format!(".{line}.")
+    }).collect();
 
-        let matches: Vec<Match> = re.captures_iter(line).map(|caps| {
-            caps.name("num").unwrap()
-        }).collect();
+    // let mut nums: Vec<Vec<Match>> = Vec::new();
+    // let mut syms: Vec<Vec<Match>> = Vec::new();
+    // let reg_num = Regex::new(r"(?<num>\d+)").unwrap();
+    // let reg_sym = Regex::new(r"(?<sym>[^\d.\n])").unwrap();
+    // for line in test.as_slice() {
+    //     // println!("Line {idx}: {line}");
 
-        nums.push(matches);
-    }
+    //     let matches: Vec<Match> = reg_num.captures_iter(line).map(|caps| {
+    //         caps.name("num").unwrap()
+    //     }).collect();
 
-    for (idx, m) in nums.iter().enumerate() {
+    //     nums.push(matches);
+
+    //     let matches: Vec<Match> = reg_sym.captures_iter(line).map(|caps| {
+    //         caps.name("sym").unwrap()
+    //     }).collect();
+
+    //     syms.push(matches);
+    // }
+
+    // let mut sum: usize = 0;
+    // for idx in 0..test.len() {
+
+        // if nums[idx].is_empty() {
+        //     continue;
+        // }
+
+        // remember, the digit starts on m.start and the last digit is m.end - 1 a.k.a. start + (len - 1)
+        // for m in nums[idx].as_slice() {
+            // if syms[idx].contains(m.start() - 1) {
+
+            // }
+        // }
+
+        // println!("Line {idx}: {}\n\tNumber Matches: {:?}\n\tSymbol Matches: {:?}", test[idx], nums[idx], syms[idx]);
+    // }
+
+    // for (idx, m) in nums.iter().enumerate() {
         // check each match from [m.start..m.end) all around for something other than digit or .
         // perhaps I could get the symbols via regex and just compare indices rather than iterating?
-    }
+
+    // }
 
 }
 
